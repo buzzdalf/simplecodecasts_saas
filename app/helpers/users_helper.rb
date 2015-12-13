1423b0c45a09c2ceeac297a6085f85a166b747d3
Bill Steinberger <buzz@buzzdalf.com>
2015-12-13 18:26:47 +0000

    Added icons to user page

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,20 @@
+module UsersHelper
+    
+    def job_title_icon
+        if @user.profile.job_title == "Developer"
+            "<i class='fa fa-code'></i>".html_safe
+        elsif @user.profile.job_title == "Entrepreneur"
+            "<i class='fa fa-lightbulb-o'></i>".html_safe
+        elsif @user.profile.job_title == "Investor"
+            "<i class='fa fa-dollar'></i>".html_safe
+        end
+    end
+    
+    def phone_number_icon
+        "<i class='fa fa-phone'></i>".html_safe
+    end
+    
+    def email_icon
+        "<i class='fa fa-envelope-o'></i>".html_safe
+    end
+end
